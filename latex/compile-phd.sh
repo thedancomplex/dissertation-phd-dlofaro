bb67c4754b546158a5babf1ef8cd274498611250
Daniel M. Lofaro <dan@danlofaro.com>
2013-01-30 18:37:34 -0500

    added compiling file

---
+++ latex/compile-phd.sh
@@ -0,0 +1,10 @@
+rm *.aux
+rm *.log
+rm *.blg
+rm *.out
+thefile=dlofaro-dissertation-phd-2013
+pdflatex $thefile.tex
+bibtex $thefile.aux
+pdflatex $thefile.tex
+pdflatex $thefile.tex
+evince $thefile.pdf
